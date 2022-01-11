348af17f2a2d62811f4fcd878895005005692ee1
garrip91 <garrip91@gmail.com>
2022-01-11 15:29:16 +0300

    Update urls.py

--- src/ManttoProject/urls.py
+++ src/ManttoProject/urls.py
@@ -26,7 +26,7 @@ from django.conf import settings
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
