4563eb28fb313b35988e10c11010111dc29622a9
garrip91 <garrip91@gmail.com>
2022-02-16 12:02:16 +0300

    Summary

--- src/static/ManttoApp/base/js/script_8.js
+++ src/static/ManttoApp/base/js/script_8.js
@@ -1,7 +1,8 @@
-/*<![CDATA[ */
+/* <![CDATA[ */
 var wpcf7 = {
     "api": {
         "root": "https:\/\/www.mantto.com.pe\/wp-json\/",
         "namespace": "contact-form-7\/v1"
     }
-}; /*]]> */
+};
+/* ]]> */
